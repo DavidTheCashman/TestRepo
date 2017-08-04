37a62e87bc25d808b34742c337be43ca9304aa10
David Cashman <david.cashman@sympa.com>
2017-08-04 12:18:49 +0300

    something something git

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -13,6 +13,17 @@ namespace TestApp
             Console.WriteLine("This is a test app");
             Console.WriteLine("This is a new line of code");
             Console.WriteLine("A third line of code added here");
+            
+            Console.WriteLine("This is code of Feature 3");
+
+            var fClass = new Feature3Class
+            {
+                Id = 1,
+                Text = "Some Text"
+            };
+
+            Console.WriteLine($"{fClass.Id} :: {fClass.Text}");
+
             Console.ReadLine();
         }
     }
